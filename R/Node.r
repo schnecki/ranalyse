38ccbfd920dbf6389630fce8d1b57b7ce322105e
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2022-04-14 12:44:41 +0200

    version 0.0.1

--- R/Node.r
+++ R/Node.r
@@ -8,34 +8,39 @@ Node <- R6::R6Class(
 
     ## Properties
     private = list(
-        .prev = NULL,     # Maybe<Node>
-        .next = NULL,     # Maybe<Node>
-        .desc = NULL      # character
+        .parent = NULL,     # Maybe<Node>
+        .childs = NULL,     # List<Node>
+        .desc = NULL        # character
     ),
 
     ## Methods
     public = list(
-        initialize = function(desc, prev) {
+        initialize = function(desc, parent) {
             self$desc <- desc
-            self$prev <- prev
+            self$parent <- parent
+        },
+        addChild = function(child) {
+            if (!("Node" %in% class(child)))
+                stop("ERROR: Unallowed property ", value, " for 'childs' at ", getSrcFilename(function(){}), ":", getSrcLocation(function(){}))
+            self.childs <- append(self.childs, list(child))
         }
     ),
 
     ## Accessable properties. Active bindings look like fields, but each time they are accessed,
     ## they call a function. They are always publicly visible.
     active = list(
-        prev = function(value) {
-            if (missing(value)) return(private$.prev)
+        parent = function(value) {
+            if (missing(value)) return(private$.parent)
             if (!("Node" %in% class(value)))
-                stop("ERROR: Unallowed property ", value, " for 'prev' at ", getSrcFilename(function(){}), ":", getSrcLocation(function(){}))
-            private$.prev <- value
+                stop("ERROR: Unallowed property ", value, " for 'parent' at ", getSrcFilename(function(){}), ":", getSrcLocation(function(){}))
+            private$.parent <- value
             return(self)
         },
-        next = function(value) {
-            if (missing(value)) return(private$.next)
+        childs = function(value) {
+            if (missing(value)) return(private$.childs)
             if (!("Node" %in% class(value)))
-                stop("ERROR: Unallowed property ", value, " for 'next' at ", getSrcFilename(function(){}), ":", getSrcLocation(function(){}))
-            private$.next <- value
+                stop("ERROR: Unallowed property ", value, " for 'childs' at ", getSrcFilename(function(){}), ":", getSrcLocation(function(){}))
+            private$.childs <- value
             return(self)
         },
         desc = function(value) {
